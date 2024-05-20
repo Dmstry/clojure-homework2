f25b4c5b2ab219fa60ae9508f0b1ebee01a24c67
Dmstry <solarr@ukr.net>
2024-05-20 22:14:18 +0300

    Resolve Problem 58, Function composition

--- src/homework2.clj
+++ src/homework2.clj
@@ -24,3 +24,24 @@
 ((decurry (fn [a]
             (fn [b]
               (* a b)))) 5 5) ;25
+
+
+;; Problem 58, Function composition
+;; Difficulty: medium
+;; Write a function which allows you to create function compositions. The parameters list should take a variable number of functions, and create a function applies them from right-to-left.
+
+(defn my-comp
+  [& funcs]
+  (fn [& args]
+    (reduce (fn [acc f]
+              (f acc))
+            (apply (last funcs) args)
+            (reverse (butlast funcs)))))
+
+((my-comp rest reverse) [1 2 3 4]) ;[3 2 1]
+
+((my-comp (partial + 3) second) [1 2 3 4]) ;5
+
+((my-comp zero? #(mod % 8) +) 3 5 7 9) ;true
+
+((my-comp #(.toUpperCase %) #(apply str %) take) 5 "hello world") ;"HELLO"
