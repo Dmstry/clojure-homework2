eac7c37434a6c66d709585f28cb80142e5293d95
Dmstry <solarr@ukr.net>
2024-05-21 02:50:22 +0300

    Resolve Problem 69, Merge with a Function

--- src/homework2.clj
+++ src/homework2.clj
@@ -45,3 +45,27 @@
 ((my-comp zero? #(mod % 8) +) 3 5 7 9) ;true
 
 ((my-comp #(.toUpperCase %) #(apply str %) take) 5 "hello world") ;"HELLO"
+
+
+;; Problem 69, Merge with a Function
+;; Difficulty: medium
+;; Write a function which takes a function f and a variable number of maps. Your function should return a map that consist of the rest of the maps conj-ed onto the first. If a key occurs in more than one map, the mapping(s) from the latter (left-to-right) should be combined with the mapping in the result by calling (f val-in-result val-in-latter).
+
+(defn my-merge-with
+  [f & maps]
+  (reduce (fn [acc map]
+            (reduce (fn [acc [k v]]
+                      (if (contains? acc k)
+                        (assoc acc k (f (acc k) v))
+                        (assoc acc k v)))
+                    acc
+                    map))
+          {}
+          maps))
+
+
+(my-merge-with * {:a 2, :b 3, :c 4} {:a 2} {:b 2} {:c 5}) ;{:a 4, :b 6, :c 20}
+
+(my-merge-with - {1 10, 2 20} {1 3, 2 10, 3 15}) ;{1 7, 2 10, 3 15}
+
+(my-merge-with concat {:a [3], :b [6]} {:a [4 5], :c [8 9]} {:b [7]}) ;{:a [3 4 5], :b [6 7], :c [8 9]}
