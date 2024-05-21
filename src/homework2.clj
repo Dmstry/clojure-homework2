41fc6338a313e46de2800b6f4254d63aefa030d7
Dmstry <solarr@ukr.net>
2024-05-22 01:28:52 +0300

    Resolve Problem 59, Juxtaposition

--- src/homework2.clj
+++ src/homework2.clj
@@ -69,3 +69,21 @@
 (my-merge-with - {1 10, 2 20} {1 3, 2 10, 3 15}) ;{1 7, 2 10, 3 15}
 
 (my-merge-with concat {:a [3], :b [6]} {:a [4 5], :c [8 9]} {:b [7]}) ;{:a [3 4 5], :b [6 7], :c [8 9]}
+
+
+;; Problem 59, Juxtaposition
+;; Difficulty: medium
+;; Take a set of functions and return a new function that takes a variable number of arguments and returns a sequence containing the result of applying each function left-to-right to the arguments.
+
+(defn my-juxt
+  [& funcs]
+  (fn [& args]
+    (map #(apply % args) funcs)))
+
+((my-juxt + max min) 2 3 5 1 6 4) ;(3 6 1)
+
+((my-juxt
+  #(.toUpperCase %)
+  count) "hello") ;("HELLO" 5)
+
+((my-juxt :a :b :c) {:a 2, :b 4, :c 6, :d 8 :e 10}) ;(2 4 6)
